52bbe6918e266494a68ad9e696c925258c3ee07c
Gary <garylai1990@gmail.com>
2012-07-24 15:09:16 +0800

    push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nckucourse::Application.config.secret_token = '47dd8688edbeae1cd3c9f40c96ba40dacf51f8786d4513ab48f3b1a0f56a626bebdb4883a003f0a60f27c88de74c18cae13e06ed4858841a93f1f7a6d5303836'
