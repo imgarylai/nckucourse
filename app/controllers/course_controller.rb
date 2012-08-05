45785e12eab7b63b4ea8fae0d3760204d67b7863
Gary Lai <garylai1990@gmail.com>
2012-08-05 16:08:44 +0800

    course controller

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,2 +1,12 @@
 class CourseController < ApplicationController
+
+  def course
+    @courses = Course.all
+
+    respond_to do |format|
+      format.html # course.html.erb
+      format.json { render json: @course }
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,9 @@ class User < ActiveRecord::Base
   def facebook
       @facebook ||= Koala::Facebook::API.new(oauth_token)
   end
+  
+  def friends
+    facebook.get_connection("me","friends")
+  end
 
 end
