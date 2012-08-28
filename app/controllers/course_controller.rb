f19706fc9712b3256a788b24e94959945aa2e652
Gary Lai <garylai1990@gmail.com>
2012-08-28 22:08:32 +0800

    add turtorial

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -22,9 +22,9 @@ class CourseController < ApplicationController
   def remove_course
     @user = User.find(session[:user_id])
     @course = Course.find(params[:id])
+    @usercourseship = @user.user_courseships.find_by_course_id(@course.id).try :destroy
     #@usercourseship = UserCourseship.where('user_id = ? and course_id = ?',
     #                                       @user, @course)[0].destroy
-    @usercourseship = @user.user_courseships.find_by_course_id(@course.id).try :destroy
     #@usercourseship = UserCourseship.where( :user_id => @user.id, :course_id => @course.id).destroy
     redirect_to :root
   end
