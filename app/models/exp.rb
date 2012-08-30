9025bbddcc683be4e09d2b6604ba7913eaac51aa
Gary Lai <garylai1990@gmail.com>
2012-08-30 22:19:34 +0800

    Launch Exp

--- app/models/exp.rb
+++ app/models/exp.rb
@@ -1,7 +1,7 @@
 class Exp < ActiveRecord::Base
   attr_accessible :content, :course_id, :material, :rate, :score, :style, :user_id, :year
   validates_presence_of :content, :course_id, :material, :rate, :score, :style, :user_id, :year
-  validates_length_of :content, :material, :style, :in => 10..800
+  validates_length_of :content, :material, :style, :in => 10..1200
   belongs_to :user
   belongs_to :course  
 end
